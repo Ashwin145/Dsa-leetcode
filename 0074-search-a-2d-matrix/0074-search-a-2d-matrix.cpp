c7a6bf0a99c8dc0c9e1217306e0989dcfe787e71
ASHWIN A.R <ashwinnair847@gmail.com>
2023-08-10 03:29:13 -0700

    Time: 0 ms (100%), Space: 9.4 MB (65.53%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -0,0 +1,27 @@
+class Solution {//Note : TC------>O(n*m)
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+       if( !matrix.size() ){
+           return false;
+       }
+       int n=matrix.size();//rows
+       int m = matrix[0].size();//columns
+       int lo = 0;
+       int h = (n*m)-1;
+       while(lo<=h){
+           int mid = (lo+(h-lo)/2);
+           if(matrix[mid/m][mid%m] == target){
+               return true;
+           }
+           else if(matrix[mid/m][mid%m]<target){
+               lo = mid+1;
+           }else{
+               h = mid-1;
+           }
+       }
+       return false;
+    
+    }
+
+    
+};
