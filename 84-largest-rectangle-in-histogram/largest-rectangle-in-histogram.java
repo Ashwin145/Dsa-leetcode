6e1d71cfc848b1c11c30feb4460705a1e98f943c
ASHWIN A.R <ashwinnair847@gmail.com>
2024-09-26 09:25:47 +0530

    Time: 111 ms (29.64%) | Memory: 57.2 MB (75.64%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -1,48 +1,47 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
-        int n = heights.length;
-        int[] nsl = new int[n]; // Next Smaller to the Left
-        int[] nsr = new int[n]; // Next Smaller to the Right
-
-        Stack<Integer> s = new Stack<>();
-
-        // Calculate NSL (Next Smaller Left)
-        for (int i = 0; i < n; i++) {
-            while (!s.isEmpty() && heights[s.peek()] >= heights[i]) {
+        int maxArea = 0;
+        int nsl[] = new int[heights.length];
+        int nsr[] = new int[heights.length];
+        
+        //Next Smaller right
+        Stack<Integer>s = new Stack<>();
+        for(int i=heights.length-1;i>=0;i--){
+            while(!s.isEmpty() && heights[s.peek()]>=heights[i]){
                 s.pop();
             }
-            if (s.isEmpty()) {
-                nsl[i] = -1; // If no smaller element, mark -1
-            } else {
-                nsl[i] = s.peek();
-            }
-            s.push(i);
-        }
-
-        s = new Stack<>(); // Clear the stack
-
-        // Calculate NSR (Next Smaller Right)
-        for (int i = n - 1; i >= 0; i--) {
-            while (!s.isEmpty() && heights[s.peek()] >= heights[i]) {
-                s.pop();
-            }
-            if (s.isEmpty()) {
-                nsr[i] = n; // If no smaller element, mark 'n' (out of bounds)
-            } else {
+            if(s.isEmpty()){
+                nsr[i] = heights.length;
+            }else{
                 nsr[i] = s.peek();
             }
             s.push(i);
         }
-
-        // Calculate the max area for each bar
-        int maxArea = 0;
-        for (int i = 0; i < n; i++) {
-            int height = heights[i];
-            int width = nsr[i] - nsl[i] - 1; // Width of the rectangle
-            int currArea = height * width; // Area = height * width
-            maxArea = Math.max(maxArea, currArea); // Update the maximum area
+        //Next Smaller left
+        s = new Stack<>();
+        for(int i=0;i<heights.length;i++){
+            while(!s.isEmpty() && heights[s.peek()]>=heights[i]){
+                s.pop();
+            }
+            if(s.isEmpty()){
+                nsl[i] = -1;
+            }else{
+                nsl[i] = s.peek();
+            }
+            s.push(i);
         }
-
+        //Max Area
+        for(int i=0;i<heights.length;i++){
+            int height = heights[i];
+            int width = nsr[i]-nsl[i]-1;
+            int currArea = height*width;
+            maxArea = Math.max(currArea,maxArea);
+             
+        }
+        //System.out.println(maxArea);
         return maxArea;
+
+        
     }
+    
 }
